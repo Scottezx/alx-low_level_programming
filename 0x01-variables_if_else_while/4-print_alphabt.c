def55f7fe8029a3e0e8d2e4d65e270eccbc55d3c
scottezx <evansnwagbo946@gmail.com>
2023-07-17 16:39:19 -0700

    when i was having that alphabet soup, i never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,21 +3,22 @@
 #include <string.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
-int main()
+int main(void)
 {
-        int i;
+	int i;
 
-        for (i = 97; i < 123; i++)
-        {
-                if (i != 101 && i != 113)
-                {
-                        putchar(i);
-                }
-        }
-        putchar('\n');
-        return (0);
+	for (i = 97; i < 123; i++)
+
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
