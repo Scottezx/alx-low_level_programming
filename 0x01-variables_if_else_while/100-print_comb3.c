7a65a339aa3cb1180aa7f856b6582b49837588bd
scottezx <evansnwagbo946@gmail.com>
2023-07-17 18:50:18 -0700

    All Advanced task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,28 +2,19 @@
 
 /**
  * main - prints all possible different combinations of two digits
- * Return: ALways 0
+ * Return: Always 0
  */
-int main()
+int main(void)
 {
-        int a, b;
+	int a = 0;
 
-        for (a = 48; a <= 56; a++)
-        {
-                for (b = 49; b <= 57; b++)
-                {
-                        if (b > a)
-                        {
-                                putchar(a);
-                                putchar(b);
-                                if (a != 56 || b != 57)
-                                {
-                                        putchar(',');
-                                        putchar(' ');
-                                }
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
+	do {
+		printf("%d", a);
+		if (a < 9)
+			printf(", ");
+		a++;
+	} while (a < 10);
+
+	printf("\n");
+	return (0);
 }
