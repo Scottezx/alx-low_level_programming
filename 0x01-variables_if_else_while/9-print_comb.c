7f70c57530d139944dd712971d94393c38d3b00f
scottezx <evansnwagbo946@gmail.com>
2023-07-17 17:21:57 -0700

    P3 makean unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - prints all possible combinations of single-digit numbers
- * Return: Always 0
+ * main - Entry point
+ * Return: 0
  */
-int main()
+int main(void)
 {
-        int n;
+	int n;
 
-        for (n = 48; n < 58; n++)
-        {
-                putchar(n);
-                if (n != 57)
-                {
-                        putchar(',');
-                        putchar(' ');
-                }
-        }
-        putchar('\n');
-        return (0);
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
 }
